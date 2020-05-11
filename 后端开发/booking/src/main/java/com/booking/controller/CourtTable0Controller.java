c362c84045523e4dc45424eec8b8d89066d6d21f
黄浩丹 <1310189872@qq.com>
2020-05-11 20:56:45 +0800

    黄浩丹初始化提交

--- 后端开发/booking/src/main/java/com/booking/controller/CourtTable0Controller.java
+++ 后端开发/booking/src/main/java/com/booking/controller/CourtTable0Controller.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.booking.pojo.CourtTable0;
@@ -20,10 +21,13 @@ public class CourtTable0Controller {
 	
 	/*获取场地*/
 	@RequestMapping("/getallCourt")
-	public List<CourtTable0> getall(){
+	public List<CourtTable0> getall(Model model){
 		List<CourtTable0> ct0list= (ct0service.selectByExample(new CourtTable0Example())) .stream().collect(Collectors.toList());
 		for(CourtTable0 ct0 : ct0list) {
-			System.out.print(ct0.toString());
+			System.out.println("测试打印");
+			System.out.print(ct0.toString()+"测试03");
+			model.addAttribute("ct0list",ct0list);
+			System.out.println("测试打印02");
 		}
 		
 		return ct0list; 
