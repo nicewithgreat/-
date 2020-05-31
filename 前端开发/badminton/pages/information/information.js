be9e23e15903e09f17c4952b2a19d0cf2f920c2b
oathzzh <2017118118@stu.hstc.edu.cn>
2020-05-31 17:35:45 +0800

    修改订场页面

--- 前端开发/badminton/pages/information/information.js
+++ 前端开发/badminton/pages/information/information.js
@@ -8,11 +8,11 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-   // wechatNO:'',
-   // avatarUrl:''
+    // wechatNO:'',
+    // avatarUrl:''
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -25,7 +25,7 @@ Page({
         //wechatNO:app.globalData.wechatNO,
         //avatarUrl:app.globalData.avatarUrl
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -47,7 +47,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -55,5 +55,5 @@ Page({
       hasUserInfo: true
     })
   }
-  
+
 })
